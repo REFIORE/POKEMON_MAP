31eec6f850bb5b9387e8d1224d7d66b4f2892b20
REFIORE <aras_andrey@bk.ru>
2025-11-16 13:18:26 +0300

    'verbose_name' for models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,20 +2,20 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200)
-    title_jp = models.CharField(max_length=200)
-    image = models.ImageField(blank=True, null=True, upload_to="images", default="image")
-    description = models.TextField(blank=True)
+    title = models.CharField(verbose_name='Название на русском', max_length=200)
+    title_en = models.CharField(verbose_name='Название на англоийском', max_length=200)
+    title_jp = models.CharField(verbose_name='Название на японском', max_length=200)
+    image = models.ImageField(verbose_name='Изображение', blank=True, null=True, upload_to="images", default="image")
+    description = models.TextField(verbose_name='Описание покемона', blank=True)
     previous_evolution = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='next_evolution', verbose_name='Предыдущая эволюция')
-    id = models.BigAutoField(primary_key=True)
+    id = models.BigAutoField(verbose_name='ID покемона', primary_key=True)
 
     def __str__(self):
         return '{}'.format(self.title)
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
     appeared_at = models.DateTimeField(verbose_name='Дата и время появления покемона', blank=True, null=True)
